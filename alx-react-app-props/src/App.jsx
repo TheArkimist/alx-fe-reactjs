930556e6638c6cb049230b1b998f187cdfd844d3
Ci$c0 <emmanueloduro37@gmail.com>
2025-11-07 16:43:12 +0000

    Context API Commit 3

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -5,9 +5,11 @@ function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <UserContext.Provider value={userData}>
-        <ProfilePage />
-    </UserContext.Provider>
+    <>
+      <UserContext.Provider value={userData}>
+           <ProfilePage />
+      </UserContext.Provider>
+    </>
   );
 }
 
---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
+const UserProfile = () => {
+
+    const userData = useContext(UserContext);
+
+    return (
+      <div>
+        <h2>{userData.name}</h2>
+        <p>Bio: {userData.email}</p>
+      </div>
+    );
+  };
+
+    export default UserProfile;
