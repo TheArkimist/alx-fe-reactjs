ef18b6dfe9255346b561792b6c23b932cb53c721
Ci$c0 <emmanueloduro37@gmail.com>
2025-11-07 12:48:20 +0000

    React New Project Second Commit

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
 import UserProfile from './components/UserProfile.jsx'
+import Counter from './components/Counter.jsx'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <Counter />
       <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
       <Footer />
     </>
