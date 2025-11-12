eb790fcad44a55ea7c07c124cb2e17431a8d06c7
Ci$c0 <emmanueloduro37@gmail.com>
2025-11-12 12:47:37 +0000

    Recipe Project Initial Commit

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,15 @@
+import AddRecipeForm from "./components/AddRecipeForm"
+import RecipeList from "./components/RecipeList"
+
+function App() {
+
+  return (
+    <>
+      <h2>I am</h2>
+      <AddRecipeForm />
+      <RecipeList />
+    </>
+  )
+}
+
+export default App
