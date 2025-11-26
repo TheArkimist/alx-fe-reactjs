6b6f2418c91a0726877b0da3cf7b2b5e6e3bf149
Ci$c0 <emmanueloduro37@gmail.com>
2025-11-26 12:30:09 +0000

    added userprofile with styles

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,12 +1,10 @@
+import UserProfile from "./components/UserProfile"
+
 function App() {
 
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-blue-600">
-          Welcome to Tailwind CSS with Vite and React!
-        </h1>
-      </div>
+      <UserProfile />
     </>
   )
 }
