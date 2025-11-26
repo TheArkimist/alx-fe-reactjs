40c11a8fb189976d95a1c14d53a8e79da56682d4
Ci$c0 <emmanueloduro37@gmail.com>
2025-11-26 12:14:05 +0000

    Tailwind project initial commit

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Welcome to Tailwind CSS with Vite and React!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
